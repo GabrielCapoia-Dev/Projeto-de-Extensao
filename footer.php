dfddafeedc8dd7e46a6de0e5f5733c2a5ac5a909
Gabriel Capoia <gabrielcapoia11@gmail.com>
2023-11-13 20:27:56 -0300

    Inicio
    Commit inicial do projeto

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>Desenvolvido por Gabriel Capoia, Gabriel Henrique e Breno Labs</p>
+</footer>
+</div>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto de Extensão</title>
+
+    <!-- Icones Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- MEU CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
+
+
+</head>
+
+<body>
+    <div class="cointainer">
+        <header class="row justify-content-end">
+            <div class="col-lg-6">
+                <a href="index.php" title="Home">
+                    <img src="#" alt="Xbox">
+                </a>
+            </div>
+
+            <nav class="col-lg-6">
+                <ul class="row justify-content-end">
+                    <li class="col"><a href="index.php" title="Home">Home</a></li>
+                    <li class="col"><a href="sobre.php" title="Sobre">Sobre</a></li>
+                    <li class="col"><a href="noticias.php" title="Noticias">Produtos</a></li>
+                    <li class="col"><a href="apoie.php" title="Apoie">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
